f649f51273923ef0bd7aa02b4f996ad1e4c4a999
entrinax22 <mark.entrina12@gmail.com>
2026-04-30 08:53:39 +0800

    Docker instead of PHP runtime

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,7 +20,10 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
+    {   
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 }
---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+php artisan migrate --force
+php artisan storage:link
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
